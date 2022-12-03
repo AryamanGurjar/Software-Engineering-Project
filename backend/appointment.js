a3872e9ae1a8da3deeda6ad83e6a3d6e2e4e5d97
Dhvani <92153529+dhvani5@users.noreply.github.com>
2022-12-03 16:29:32 +0530

    Chatbot and doc page

--- backend/appointment.js
+++ backend/appointment.js
@@ -10,6 +10,14 @@ const firebaseApp = firebase.initializeApp({
 const database = firebaseApp.firestore();
 // const auth = firebaseApp.auth();
 
+// service cloud.firestore {
+//     match /databases/{database}/documents {
+//       match /{document=**} {
+//         allow read, write: if request.auth != null;
+//       }
+//     }
+//   }
+
 const signUp = () => {
     const name = document.getElementById("name").value;
     const date = document.getElementById("date").value;
@@ -34,10 +42,46 @@ const signUp = () => {
 
 }
 
+const addDoc = () => {
+    const name = document.getElementById("name").value;
+    const date = document.getElementById("date").value;
+    const timeslots1 = document.getElementById("timeslots1").value;
+    const timeslots2 = document.getElementById("timeslots2").value;
+    const timeslots3 = document.getElementById("timeslots3").value;
+    const timeslots4 = document.getElementById("timeslots4").value;
+    // const duration = document.getElementById("duration").value;
+
+    const obj = {
+        name: name,
+        date: date,
+        timeslots1: timeslots1,
+        timeslots2: timeslots2,
+        timeslots3: timeslots3,
+        timeslots4: timeslots4
+        // duration: duration
+    };
+
+    database.collection("DocData").add(obj).then((response)=>{
+        console.log(response.id);
+    }).catch((e)=>{
+        console.log(e);
+        console.log(e.message);
+    });
+
+}
+
 const fetch = () => {
     database.collection("userData").get().then((response) => {
         response.forEach((doc) => {
             console.log("Patient name: " + doc.data().name + "\nDate: " + doc.data().date + "\nHaving symptoms: " + doc.data().symptoms);
+        });
+    });
+}
+
+const fetchDoc = () => {
+    database.collection("DocData").get().then((response) => {
+        response.forEach((doc) => {
+            checkSlots.html.log("Doctor name: " + doc.data().name + "\nDate: " + doc.data().date + "\nTime Slots: " + doc.data().timeslots1 + doc.data().timeslots2 );
         });
     });
 }
